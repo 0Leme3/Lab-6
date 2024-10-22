93a9ee65ec63784e625e31457693a3ee80e44098
gdsme <diego.mendez@ufl.edu>
2024-10-22 15:36:43 -0400

    This is Diego's initial code

---
+++ lab6.py
@@ -0,0 +1,15 @@
+#Diego Mendez
+def encode(toEncode):
+    encoded = ""
+    for i in toEncode:
+        if int(i)<=6:
+            encoded += str(int(i) + 3)
+        elif int(i)>6:
+            encoded += str(int(i) - 7)
+    return encoded
+
+
+
+if __name__ == '__main__':
+    encoded_string = encode("00009962")
+
